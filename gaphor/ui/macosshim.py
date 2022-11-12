511056b22aa16489bb1685a3f801f23e41f38348
Arjan Molenaar <gaphor@gmail.com>
2022-11-12 16:13:40 +0100

    Do a platform check for macos shim
    So we can do tweaks for GTK4 as well (if needed).
    It;s now more similar to the windows tweaks.

--- gaphor/ui/macosshim.py
+++ gaphor/ui/macosshim.py
@@ -1,17 +1,16 @@
-try:
-    import gi
-    from gi.repository import Gtk
+import logging
+import sys
 
-    if Gtk.get_major_version() == 3:
-        gi.require_version("GtkosxApplication", "1.0")
-    else:
-        raise ValueError()
-except ValueError:
+from gi.repository import Gtk
+
+log = logging.getLogger(__name__)
+
+if sys.platform != "darwin":
     macos_init = None
-else:
-    from gi.repository import GtkosxApplication
+elif Gtk.get_major_version() == 3:
+    import gi
 
-    macos_app = GtkosxApplication.Application.get()
+    macos_app = None
 
     def open_file(macos_app, path, application):
         if path == __file__:
@@ -26,7 +25,26 @@ else:
         return not quit
 
     def macos_init(application):
+        try:
+            gi.require_version("GtkosxApplication", "1.0")
+        except ValueError:
+            log.warning("GtkosxApplication not found")
+            return
+
+        from gi.repository import GtkosxApplication
+
+        global macos_app
+        if macos_app:
+            return
+
+        macos_app = GtkosxApplication.Application.get()
+
         macos_app.connect("NSApplicationOpenFile", open_file, application)
         macos_app.connect(
             "NSApplicationBlockTermination", block_termination, application
         )
+
+else:
+
+    def macos_init(application):
+        pass
